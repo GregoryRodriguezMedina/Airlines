c3bcd1fa915801e8a967db4311da38b084975123
Gregory Rodriguez <gregoryrodriguez@outlook.com>
2022-08-18 16:04:09 -0400

    Agregando nuevas apis al proyecto, codigo por defecto

--- Airlines.Apis/Program.cs
+++ Airlines.Apis/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<Airlines.Apis.Data.AirlinesContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("AirlinesConnection"));
+    });
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
